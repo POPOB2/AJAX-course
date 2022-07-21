fe45a48a22128562110b87412a0bdde026f1d65f
PB2 <nft192891@gmail.com>
2022-07-21 16:26:57 +0800

    處理查詢資料的請求

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,17 @@
 <?php
 //處理查詢資料的請求
 
+$classroom=$_GET['classroom'];// 從api/query.php  'get'過來的值 在此用$_GET接
+
+$dsn="mysql:host=localhost;charset=utf8;dbname=students";
+$pdo=new PDO($dsn,'root','');
+
+$sql="SELECT * FROM students WHERE classroom='{$classroom}'";
+$rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+
+// print_r($rows);
+echo json_encode($rows);
+
 
 ?>
