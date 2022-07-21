f7968777aa914976b1c05cb9c4245c2e84be8cd6
PB2 <nft192891@gmail.com>
2022-07-21 16:27:32 +0800

    處理刪除資料的請求

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -1,5 +1,13 @@
 <?php
 //處理刪除資料的請求
+$id=$_POST['id']; // 接收POST過來的id
+$dsn="mysql:host=localhost;charset=utf8;dbname=students";
+$pdo=new PDO($dsn,'root','');
+
+$sql="DELETE FROM students WHERE id='$id'";
+
+echo $pdo->exec($sql); // 從$pdo-> 執行(執行的內容)
+// 若有刪成功得1 反之失敗為0或null
 
 
 ?>
